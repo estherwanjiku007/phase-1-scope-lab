e5b048f32bf4086eb03dad6d664fb4a29537e1bb
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2023-12-14 18:37:40 +0300

    aster

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName="bob";
+var bestCustomer;
+//const leastFavouriteCustomer="jean";
+function upperCaseCustomerName(){
+customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer (){
+      bestCustomer="not bob";
+     
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+}
+const leastFavoriteCustomer="jean";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Assignment to constant variable";
+}
+changeLeastFavoriteCustomer();
